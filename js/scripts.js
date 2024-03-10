cfc309ca7a0f004c71f6a45864793764532705c6
Samantha <158760603+samanthasbytes@users.noreply.github.com>
2024-03-10 18:01:05 -0400

    Task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
-alert('Hello World');
+// Create a new variable called pokemonList and assign to it a blank array. This array contains Pokémon data to display in your application.
 
-let favoriteFood = 'banana';
+// Add 3 Pokémon objects to the pokemonList array. Each object represents one Pokémon and needs to have the same keys to avoid errors.
 
-document.write(favoriteFood);
+/* Add the following three keys to each Pokémon object and make up a value for each of them
+  name (string, e.g., "Bulbasaur")
+  height (number, e.g., 7)
+  types (array of strings, e.g., ['grass', 'poison'])
+  
+  You can use Pokedex.org as a reference.
+*/
+
+let pokemonList = ['Charizard', 'Jigglypuff', 'Psyduck']
+
+let Charizard = {
+    name: 'Charizard',
+    height: 1.7,
+    types: ['fire', 'flying']
+};
+
+let Jigglypuff = {
+    name: 'Jigglypuff',
+    height: 0.5,
+    types: ['fairy', 'normal']
+};
+
+let Psyduck = {
+    name: 'Psyduck',
+    height: 0.8,
+    types: 'water'
+};
