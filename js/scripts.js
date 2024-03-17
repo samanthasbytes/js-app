43748fea02b13a555146a1c564c0c961f640f57e
Samantha <158760603+samanthasbytes@users.noreply.github.com>
2024-03-17 11:46:59 -0400

    Psyduck "types" is an array rather than a string
    Removed instructions comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-// Create a new variable called pokemonList and assign to it a blank array. This array contains Pokémon data to display in your application.
-
-// Add 3 Pokémon objects to the pokemonList array. Each object represents one Pokémon and needs to have the same keys to avoid errors.
-
-/* Add the following three keys to each Pokémon object and make up a value for each of them
-  name (string, e.g., "Bulbasaur")
-  height (number, e.g., 7)
-  types (array of strings, e.g., ['grass', 'poison'])
-  
-  You can use Pokedex.org as a reference.
-*/
-
 let pokemonList = [
   {
     name: 'Charizard',
@@ -26,6 +14,6 @@ let pokemonList = [
   {
     name: 'Psyduck',
     height: 0.8,
-    types: 'water',
+    types: ['water'],
   },
 ];
