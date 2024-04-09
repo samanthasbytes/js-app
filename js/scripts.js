347bc01cbe8f57cccbecf6bdab449afe4c5ba07b
Samantha <158760603+samanthasbytes@users.noreply.github.com>
2024-04-09 02:01:22 -0400

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
-let pokemonList = [
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire', 'flying'],
-  },
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying'],
+    },
 
-  {
-    name: 'Jigglypuff',
-    height: 0.5,
-    types: ['fairy', 'normal'],
-  },
+    {
+      name: 'Jigglypuff',
+      height: 0.5,
+      types: ['fairy', 'normal'],
+    },
 
-  {
-    name: 'Psyduck',
-    height: 0.8,
-    types: ['water'],
-  },
-];
+    {
+      name: 'Psyduck',
+      height: 0.8,
+      types: ['water'],
+    },
+  ];
 
-pokemonList.forEach(function (pokemon) {
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   if (pokemon.height >= 1.7) {
     document.write(
-      pokemon.name + ` (height: ${pokemon.height}) - Wow, that's big!<br/>`
+      pokemon.name + ` (height: ${pokemon.height}) - Wow, that's big!<br>`
     );
   } else {
-    document.write(pokemon.name + ` (height: ${pokemon.height})<br/>`);
+    document.write(pokemon.name + ` (height: ${pokemon.height})<br>`);
   }
 });
