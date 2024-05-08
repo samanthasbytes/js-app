ccf2be538d288259f0492ca7596be944ce4bccf6
Samantha <158760603+samanthasbytes@users.noreply.github.com>
2024-05-08 14:24:00 -0400

    Adds an "X" SVG as a close button in modal

--- js/scripts.js
+++ js/scripts.js
@@ -118,7 +118,8 @@ let modalRepository = (function () {
     // add the modal content: close button, title, content, image
     let closeButtonElement = document.createElement('button');
     closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
+    // closeButtonElement.innerText = 'Close';
+    closeButtonElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>'; // prompted ChatGPT to generate SVG code for a simple black "X"
     closeButtonElement.addEventListener('click', hideModal);
 
     let titleElement = document.createElement('h1');
